a632e0227d7a49ca0fbc8da5077e4bcb7d015fa9
Anna Glowik <annaglowik@gmail.com>
2017-10-21 14:46:24 +0200

    javascript

---
+++ js/app.js
@@ -0,0 +1,57 @@
+//console.log("działa!");
+////liczby i stringi - tekt
+//var myName = "Ania";
+//console.log(myName);
+//var number1 = 56;
+////wartości logiczne
+//var prawda = true;
+//var falsz = false;
+//console.log(prawda);
+//// Tablice, w tablicach elementy liczymy od zera
+//var tablica = [1,2,3];
+//console.log(tablica[1]);
+////Pobieranie dlugosci tablicy
+//tablica.length;
+//console.log(tablica.length);
+//var tablica2 = ["banan","mango","truskawka"];
+//console.log(tablica2);
+//console.log(tablica2[0]);
+////FUNKCJE
+//function wypisywanie(warzywo){console.log(warzywo);
+//                            var sok = "miksowanie" + warzywo;
+//                            return sok;
+//                             }
+//var otrzymanySok = wypisywanie("marchew");   //uruchamianie funkcji poprzez wypisanie jej nazwy ktora nadalismy -warzywo
+//console.log(wypisywanie("rzodkiew"));
+//wypisywanie("ziemniak");
+//console.log(otrzymanySok)
+////INSTRUKCJE WARUNKOWE
+//var czyPada = true;
+//if(czyPada){
+//    console.log("wez parasol")
+//}
+//else{console.log("wez okulary przeciwsloneczne")}
+////PETLE
+//for(var i = 98; i < 100; i++){
+//console.log("czesc", i)    
+//}
+//CWICZENIA
+var myName = "Ania";
+console.log(myName);
+var tablica = ["Maria","Ania","Kasia","Marta"];
+console.log(tablica[1]);
+var myNumber1 = 13;
+var myNumber2 = 7;
+if(myNumber1>myNumber2){
+    console.log("hurra");
+}
+for(var i= 0; i<4; i++){
+    console.log(tablica[i])
+    
+}
+//DALEJ WYKLAD
+//DOM Api - Document Object Model
+var divs = document.querySelectorAll("div")
+undefined
+divs
+(24) [div.col-sm-4.brand-container, div.collapse.navbar-collapse.col-sm-8, div.dropdown, div.jumbotron, div.container, div.container-fluid.main-points, div.row, div.plan, div.row, div.plan-description.col-sm-6.col-sm-offset-1, div.list-container, div.input-group, div.orgs, div.row, div.col-sm-6.col-sm-offset-3, div.row, div.col-sm-6.col-sm-offset-3.orgs-container, div.col-sm-6, div.col-sm-6, div.dots.col-sm-4.col-sm-offset-4, div.dot.active, div.dot, div.dot, div.col-sm-4]
